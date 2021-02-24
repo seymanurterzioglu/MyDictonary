2ae9151849ec9fe2115634c1be8beec4e3df033f
Şeyma Nur Terzioğlu <nightotaku00@hotmail.com>
2021-02-24 22:33:56 +0300

    Gun 4/Odev 5

---
+++ MyDictonary/MyDictonary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictonary<TKey, TValue>
+    {
+        //List olarak tutulması dizi olarak tutulmasında daha iyi
+
+        List<TKey> keys;
+        List<TValue> values;
+        public MyDictonary()
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public void Control(TKey arakey)
+        {
+            //indexof ile gelen arakey in keys deki yerini (index ini) buluyoruz.ve values listinin bu index deki degerini yazdırıyoruz.
+            Console.WriteLine(keys.IndexOf(arakey) + ".Key's Value= "+values[keys.IndexOf(arakey)]);
+        }
+
+    }
+}
--- MyDictonary/Program.cs
+++ MyDictonary/Program.cs
@@ -6,7 +6,24 @@ namespace MyDictonary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDictonary<int,string> uyeler= new MyDictonary<int, string>();
+
+            uyeler.Add(10,"Seyma");
+            uyeler.Add(13, "Salih");
+            uyeler.Add(14, "Semih");
+            uyeler.Add(17, "Suna");
+            uyeler.Add(21, "Sena");
+            uyeler.Add(25, "Selim");
+
+            //Verileri dogru aktarabildigimizi kontrol icin;
+
+            uyeler.Control(10);
+            uyeler.Control(13);
+            uyeler.Control(14);
+            uyeler.Control(17);
+            uyeler.Control(21);
+            uyeler.Control(25);
+
         }
     }
 }
